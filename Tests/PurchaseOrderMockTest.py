4648504ada97ad8a15dcf5b2d137b48464115468
jesusjgs <jesusjgs@alu.uma.es>
2018-05-26 21:08:42 +0200

    Structure changed and new test case

--- Tests/PurchaseOrderMockTest.py
+++ Tests/PurchaseOrderMockTest.py
@@ -1,8 +1,11 @@
-
 import unittest
+
 from unittest.mock import MagicMock
 
+from mockito import mock, verify
+
 from PurchaseOrder import PurchaseOrder
+
 from Store import Store
 
 
@@ -10,34 +13,45 @@ class TestMethods(unittest.TestCase):
 
     def test_should_buy_remove_the_product_in_the_store_if_there_are_enough_items(self):
         # Creating the mock object
-        store:Store = MagicMock()
+        store: Store = MagicMock()
 
         # Stubbing
         store.are_there_enough_products.return_value = True
 
         # Execute
-        purchase_order:PurchaseOrder = PurchaseOrder("Bread", 15)
+        purchase_order: PurchaseOrder = PurchaseOrder("Bread", 15)
         purchase_order.buy(store)
 
         # Verify
         store.remove_products.assert_called_once_with("Bread", 15)
-        #store.remove_products.assert_not_called("Pork", 12)
+        store.remove_products.assert_not_called("Pork", 12)
 
     def test_should_buy_not_remove_the_product_in_the_store_if_there_are_not_enough_items(self):
         # Creating the mock object
-        store:Store = MagicMock()
+        store: Store = MagicMock()
 
         # Stubbing
         store.are_there_enough_products.return_value = False
 
         # Execute
-        purchase_order:PurchaseOrder = PurchaseOrder("Bread", 15)
+        purchase_order: PurchaseOrder = PurchaseOrder("Bread", 15)
         purchase_order.buy(store)
 
         # Verify
         store.remove_products.assert_not_called()
 
     def test_shouldBuyWorkIfThereAreProductsTheFirstTimeButNotTheSeconeOne(self):
-        # To be implemented
-        pass
+        # Creating the mock object
+        store: Store = MagicMock()
 
+        # Stubbing
+        store.are_there_enough_products.return_value = True
+
+        # Execute
+        purchase_order: PurchaseOrder = PurchaseOrder("Bread", 15)
+        purchase_order.buy(store)
+        purchase_order.buy(store)
+
+        # Verify
+        verify(store, times=2).are_there_enough_products()
+        verify(store, times=1).remove_Products("Bread", 15)
